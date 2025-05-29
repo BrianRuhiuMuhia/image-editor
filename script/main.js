65f548f00ecebf0d256c64903c60cc8cafd824b1
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2025-05-29 12:17:39 +0300

    buttons

--- script/main.js
+++ script/main.js
@@ -13,24 +13,129 @@ const downloadB=document.querySelector(".download")
 const rangeInput=document.querySelector(".range")
 const rangeBtn=document.querySelector(".light-btn")
 const buttonContainer=document.querySelectorAll(".btn-container")
+const red=document.querySelector(".red")
+const blue=document.querySelector(".blue")
+const green=document.querySelector(".green")
+const opacity=document.querySelector(".opacity")
+const rangeBlue=document.querySelector(".range-blue")
+const rangeRed=document.querySelector(".range-red")
+const rangeGreen=document.querySelector(".range-green")
+const rangeOpacity=document.querySelector(".range-opacity")
+const blackWhite=document.querySelector(".black-white")
+const magnitude=5
 const canvasStates=[]
 let isImageLoaded=false
 let isDrawing=false
 let checkDrawing=false
+function getCurrentImage(){
+  const newImage=new Image()
+  newImage.src=canvas.toDataURL()
+  return newImage
+}
 sBtn.addEventListener("click",(event)=>{
 event.preventDefault()
 toggleSelectBox()
 
 })
 rangeBtn.addEventListener("click",rangeBtnFunc)
-let inputValues={"current":null,"previous":null} 
+const inputValues={"current":null,"previous":null} 
+opacity.addEventListener("click",function(){
+  rangeOpacity.classList.toggle("hidden")
+  checkDrawing=false
+})
+red.addEventListener("click",function(){
+  rangeRed.classList.toggle("hidden")
+  checkDrawing=false
+})
+blue.addEventListener("click",function(){
+  rangeBlue.classList.toggle("hidden")
+  checkDrawing=false
+})
+green.addEventListener("click",function(){
+  rangeGreen.classList.toggle("hidden")
+  checkDrawing=false
+})
+rangeGreen.addEventListener("input",function(event){
+  inputValues["previous"]=inputValues["current"]
+  inputValues["current"]=parseInt(event.target.value)
+  let dir=checkDirection(inputValues) ? 1:-1
+  
+  changeGreen(magnitude,dir)
+})
+rangeRed.addEventListener("input",function(event){
+  inputValues["previous"]=inputValues["current"]
+  inputValues["current"]=parseInt(event.target.value)
+  let dir=checkDirection(inputValues) ? 1:-1
+  changeRedness(magnitude,dir)
+})
+rangeBlue.addEventListener("input",function(event){
+  inputValues["previous"]=inputValues["current"]
+  inputValues["current"]=parseInt(event.target.value)
+  let dir=checkDirection(inputValues) ? 1:-1
+  changeBlue(magnitude,dir)
+})
+rangeBlue.addEventListener("input",function(event){
+  inputValues["previous"]=inputValues["current"]
+  inputValues["current"]=parseInt(event.target.value)
+  let dir=checkDirection(inputValues) ? 1:-1
+  changeOpacity(magnitude,dir)
+})
+function changeRedness(value,dir){
+  const {image,data}=getImageData()
+  for(let i=0;i<data.length;i+=4){
+checkRange(data[i]) ? data[i]=255:data[i]+=value*dir
+   
+  }
+  imageDataToCanvas({image,data})
+}
+let count=0
+function changeToblackWhite(){
+  const {image,data}=getImageData()
+  if(count>=1)
+  {
+    return
+  }
+  for(let i=0;i<data.length;i+=4){
+    checkRange(data[i]) ? data[i]=255:data[i]*=0.299
+    checkRange(data[i+1]) ? data[i+1]=255:data[i+1]*=0.587
+    checkRange(data[i+2]) ? data[i+2]=255:data[i+2]*=0.114
+    data[4]=255
+  }
+  imageDataToCanvas({image,data})
+  count+=1
+}
+blackWhite.addEventListener("click",changeToblackWhite)
+function changeBlue(value,dir){
+  const {image,data}=getImageData()
+  for(let i=0;i<data.length;i+=4){
+    checkRange(data[i+3]) ? data[i+3]=255:data[i+3]+=value*dir
+  }
+  imageDataToCanvas({image,data})
+}
+function changeGreen(value,dir){
+  const {image,data}=getImageData()
+  
+  for(let i=0;i<data.length;i+=4){
+    checkRange(data[i+2]) ? data[i+2]=255:data[i+2]+=value*dir
+    data[i+2]+=value*dir
+  }
+  imageDataToCanvas({image,data})
+}
+function changeOpacity(value,dir){
+  const {image,data}=getImageData()
+  console.log(data.length)
+  for(let i=0;i<data.length;i+=4){
+    checkRange(data[i+4]) ? data[i+4]=255:data[i+4]+=value*dir
+    
+  }
+  imageDataToCanvas({image,data})
+}
 rangeInput.addEventListener("input",(event)=>{
   
   if(isImageLoaded){
       inputValues["previous"]=inputValues["current"]
   inputValues["current"]=parseInt(event.target.value)
   let dir=checkDirection(inputValues) ? 1:-1
-  const magnitude=1
   changeImageBrightness(magnitude,dir)
   }
   else{
@@ -39,14 +144,21 @@ rangeInput.addEventListener("input",(event)=>{
   }
 
 })
+function checkRange(color){
+return color>=255
+}
 buttonContainer.forEach((container)=>{
   const span=container.querySelector("span")
-  container.addEventListener("mouseover",()=>{
+  if(span!=undefined)
+  {
+    container.addEventListener("mouseover",()=>{
     span.classList.replace("hidden","display")
   })
   container.addEventListener("mouseleave",()=>{
     span.classList.replace("display","hidden")
   })
+  }
+  
 
 })
 function checkDirection(obj)
@@ -186,17 +298,26 @@ function loadImage(file){
   } 
 function changeImageBrightness(value,dir)
 {
-  const image=gCtx.getImageData(0,0,canvas.width,canvas.height)
-  const imageData=image.data
-  for(let i=0;i<imageData.length;i++){
-imageData[i]+=value*dir
-imageData[i+1]+=value*dir
-imageData[i+2]+=value*dir
+  const {image,data}=getImageData()
+  for(let i=0;i<data.length;i++){
+checkRange(data[i]) ? data[i]=255:data[i]+=value*dir
+checkRange(data[i+1]) ? data[i+1]=255:data[i+1]+=value*dir
+checkRange(data[i+2]) ? data[i+2]=255:data[i+2]+=value*dir
   }
-const newImageData = new ImageData(imageData, image.width, image.height);
+imageDataToCanvas({image,data})
+}
+function getImageData()
+{
+  const image = gCtx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = image.data;
+  const imageData={image,data}
+  return imageData
+}
+function imageDataToCanvas({image,data}){
+  const newImageData = new ImageData(data, image.width, image.height);
   gCtx.putImageData(newImageData, 0, 0)
   image.src = canvas.toDataURL();
-  canvasStates.push(gCtx.getImageData(0, 0, canvas.width, canvas.height));
+  saveState()
 }
 function showImageIcons(image){
     let canv=document.createElement("canvas")
@@ -219,29 +340,41 @@ function showImageIcons(image){
     icons.insertBefore(lastIcon, clickedNext);
     icons.insertBefore(clickedIcon, lastNext);
     const imageDataURL = clickedIcon.toDataURL();
-  
+  const newImage=new Image()
+  newImage.src=imageDataURL
+  newImage.onload=()=>{
+    reDrawCanvas(newImage)
+
+  }
   }
 
 icons.addEventListener("click",changePos)
 function reDrawCanvas(image){
   drawImage(canvas,image)
 }
-function drawImage(canvas,image){
-  if(!image){
-    return ""
+function drawImage(canvas, image, degrees) {
+  if (!image) {
+    throw new Error("No Image");
+  } else {
+    const ctx = canvas.getContext("2d");
+    clearCanvas(ctx);
+    ctx.save();
+    const radians = degrees * Math.PI / 180;
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate(radians);
+    ctx.drawImage(image, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+    ctx.restore();
+    saveState()
   }
-  else{
-    const ctx=canvas.getContext("2d")
-    clearCanvas(ctx)
-    ctx.drawImage(image,0,0,canvas.width,canvas.height)
-  }
-
-  }
+}
   function clearCanvas(ctx){
-        
  ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
   function genRandomImageName(){
     return Math.random().toString(36).substr(2, 9);
+  }
+  function saveState()
+  {
+     canvasStates.push(gCtx.getImageData(0, 0, canvas.width, canvas.height));
   }
 
